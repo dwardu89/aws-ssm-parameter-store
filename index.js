95ebff670c22a56c63bc1d92f333916a08da0f33
Edward Vella <hello@dwardu.com>
2021-02-01 18:46:39 +0000

    Adding test case to check that parameter is added

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+var AWS = require('aws-sdk');
 const { SSM } = require("@aws-sdk/client-ssm");
 
 try {
+    // Set the region 
+    AWS.config.update({ region: core.getInput('aws-region') });
     // Load the AWS Region to use in SSM
-    const ssm = new SSM({ region=core.getInput('aws-region') })
+    const ssm = new SSM()
     var ssm_path_name = core.getInput('ssm-path')
     var params = {
-        Name=ssm_path_name,
-        Value=core.getInput('ssm-value'),
-        Type=core.getInput('ssm-value-type'),
-        Overwrite=core.getInput('ssm-value-overwrite')
+        Name: ssm_path_name,
+        Value: core.getInput('ssm-value'),
+        Type: core.getInput('ssm-value-type'),
+        Overwrite: core.getInput('ssm-value-overwrite')
     }
     ssm.putParameter(params).then(value => {
         console.log(`Storing Variable in path [${ssm_path_name}]`);
