43cd180713cd2eb402010285eff4eb1e087f3f96
OfirYaron <ofirya@gmail.com>
2023-10-21 21:18:57 +0300

    removal of required value if skip is true

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ async function run() {
       apiVersion: "2014-11-06",
       region: core.getInput("aws-region"),
     });
+    var skip = core.getInput("skip-empty-value", { required: false });
     var params = {
       Name: core.getInput("ssm-path", { required: true }),
-      Value: core.getInput("ssm-value", { required: true }),
+      Value: core.getInput("ssm-value", { required: !skip }),
       Type: core.getInput("ssm-value-type", { required: true }),
       Overwrite: core.getBooleanInput("ssm-value-overwrite", {
         required: true,
       }),
       Description: core.getInput("ssm-value-description"),
     };
-    var skip = core.getInput("skip-empty-value", { required: false });
     if (skip === true && params.Value === '') {
       core.info(`Skipping empty value parameter in path [${ssm_path}]`);
       return;
